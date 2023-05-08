05c6ddc765da4e2d4d27dae5806abcd60f50dc9f
kmctown <chic@hey.com>
2023-05-08 15:46:25 -0400

    Update pytest-asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 from typing import AsyncGenerator
-import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 
 from app.main import app
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
